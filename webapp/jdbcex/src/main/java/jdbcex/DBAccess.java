700bb01acb432f4820b17204e3893e34f64712bf
kpodlaski <podlaski@uni.lodz.pl>
2019-11-21 17:14:27 +0100

    access v.1 ready

--- webapp/jdbcex/src/main/java/jdbcex/DBAccess.java
+++ webapp/jdbcex/src/main/java/jdbcex/DBAccess.java
@@ -13,20 +13,22 @@ public class DBAccess {
             ("jdbc:sqlite:C:\\tmp\\workshop_db.db");
     }
 
-    public Object getStudentWithId(int id){
+    public Student getStudentWithId(int id) throws SQLException{
         Statement st = dCon.createStatement();
         String sql = "SELECT * FROM STUDENT WHERE ID = "+id;
         st.execute(sql);
         ResultSet rs = st.getResultSet();
+        Student student = null;
         while(rs.next()){
             int sid = rs.getInt(1);
             String name = rs.getString("NAME");
             String sname = rs.getString(3);
             String email = rs.getString(4);
-            
+            student = new Student(sid,name,sname,email);
             //out.println(id+" "+name+" "+sname +" "+email);
             //out.println("<br>");
         }
+        return student;
 }
     }
 
